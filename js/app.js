b130ceab9d7cc3d30640a8fbcdd5681ccf2f96ea
Jane Maguire <jane.m.maguire@gmail.com>
2016-10-09 14:42:43 +0100

    Making images as objects

--- js/app.js
+++ js/app.js
@@ -1,9 +1,57 @@
-
+document.addEventListener("DOMContentLoaded", function() {
 // randomly lay out images
 
-images = ["red", "blue", "green", "yellow", "pink", "purple"];
+//lists but not randomly
 
-for (i=0; i<12; i++) {
-  var randomImages = images[Math.floor(Math.random) * 6];
-  console.log(images[i]);
+// images = ["red", "blue", "green", "yellow", "pink", "purple"];
+
+// for (i=0; i<12; i++) {
+//   var randomImages = images[Math.floor(Math.random) * 6];
+//   console.log(images[i]);
+// }
+//
+// images = ["red", "blue", "green", "yellow", "pink", "purple"];
+//
+// for (i=0; i<12; i++) {
+//   var randomImages = images[Math.floor(Math.random) * 6];
+//   console.log(images[i], images[i], images[i]);
+// }
+
+
+// console.log(images[Math.floor(Math.random) * 12]);
+
+// console.log(images[3]);
+
+// function getRandomImage() {
+//   for (i=0; i<12; i++) {
+//     console.log(images[Math.floor(Math.random() * images.length-1)]);
+//     }
+//   }
+
+// getRandomImage(images);
+//
+// function getRandomImage() {
+//   for (i=0; i<12; i++) {
+//     document.getElementById('a').innerHTML = (images[Math.floor(Math.random() * images.length-1)]);
+//     }
+//   }
+//
+// getRandomImage(images);
+
+
+// document.getElementsByClass('.button').innerHTML ="red";
+
+// var a = document.getElementById('a');
+//
+// a.innerHTML = 'hello';
+
+function Image() {
+  this.name = name;
+  this.src = src;
 }
+
+trousers = new Object ();
+image.name = 'trousers';
+image.src = 'images/trousers.html';
+
+});
