7fc80ce78e68611c1ba3fb77f200565cbde5c47c
Jane Maguire <jane.m.maguire@gmail.com>
2016-10-11 12:23:26 +0100

    Added back to cards

--- js/app.js
+++ js/app.js
@@ -59,57 +59,96 @@ document.addEventListener("DOMContentLoaded", function() {
   //
   // document.getElementsByClass('button').innerHTML = '';
 
+  function shuffle(array) {
+    var m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+
+    return array;
+  }
+
   var wrapper = document.getElementById('wrapper');
   var images = ["alien", "duke", "eyes", "labyrinth", "jack", "ziggy", "blackstar", "coat", "alien", "duke", "eyes", "labyrinth", "jack", "ziggy", "blackstar", "coat"];
+  images = shuffle(images);
+  console.log(images);
   var counter = 1;
+  var moves = [];
+  var clickedElements = [];
 
   // create buttons
 
   for (i=0; i<images.length; i++) {
     var newButton = document.createElement("button");
-    // newButton.setAttribute('data-img', images[i]);
+    newButton.setAttribute('data-img', images[i]);
     newButton.addEventListener('click', function() {
+      var selection = this.getAttribute('data-img');
 
+      moves.push(selection);
+      clickedElements.push(this);
+      console.log("MOVES:", moves);
       if (!this.innerHTML) {
+        this.innerHTML = "<a href='#'><img src='images/" + selection + ".jpg'></a>";
+        // var randomIndex = Math.floor(Math.random() * images.length-1);
+        // var randomImage = images.splice(randomIndex, 1);
 
-      var randomIndex = Math.floor(Math.random() * images.length-1);
-      var randomImage = images.splice(randomIndex, 1);
+        if (counter % 2 === 0 ) {
+          if(moves[0] === moves[1] ) {
+            console.log("match");
+            moves = [];
+            clickedElements = [];
 
-      if (counter % 2 === 0 ) {
-        console.log("second click");
-      } else {
-        console.log("first click");
+          } else {
+            setTimeout(function() {
+              clickedElements[0].innerHTML = "";
+              clickedElements[1].innerHTML = "";
+              moves = [];
+              clickedElements = [];
+            }, 1000);
+          }
+
+        } else {
+          console.log("first click");
+        }
+
+        counter++;
       }
-      this.innerHTML = "<a href='#'><img src='images/" + randomImage + ".jpg'></a>";
-      counter++;
-    }
     });
     wrapper.appendChild(newButton);
   }
 
-// Insert an image in the DOM
+  // Insert an image in the DOM
 
-// var result = document.getElementById('result');
-// result.innerHTML = "<img src='https://1.bp.blogspot.com/-MpnaAEObdws/VyfEFRLtbyI/AAAAAAAADxM/bDJN44Vvgf0u3KxcqnY2gcPzLmTofPDPwCKgB/s1600/29.%2B%25E2%2580%259CI%2Bdon%2527t%2Bknow%2Bwhere%2BI%2527m%2Bgoing%2Bfrom%2Bhere%252C%2Bbut%2BI%2Bpromise%2Bit%2Bwon%2527t%2Bbe%2Bboring.%25E2%2580%259D%2B%25E2%2580%2595%2BDavid%2BBowie.JPG'>";
+  // var result = document.getElementById('result');
+  // result.innerHTML = "<img src='https://1.bp.blogspot.com/-MpnaAEObdws/VyfEFRLtbyI/AAAAAAAADxM/bDJN44Vvgf0u3KxcqnY2gcPzLmTofPDPwCKgB/s1600/29.%2B%25E2%2580%259CI%2Bdon%2527t%2Bknow%2Bwhere%2BI%2527m%2Bgoing%2Bfrom%2Bhere%252C%2Bbut%2BI%2Bpromise%2Bit%2Bwon%2527t%2Bbe%2Bboring.%25E2%2580%259D%2B%25E2%2580%2595%2BDavid%2BBowie.JPG'>";
 
-//
-// function getRandomImage() {
-//   for (i=0; i<images.length; i++) {
-//     console.log(images[Math.floor(Math.random() * images.length-1)]);
-//   }
-// }
-// randomImageArray =[];
-//
-// var setTiles = function () {
-//   for (var i=0; i<images.length; i++) {
-//     var imageLocation = ((Math.floor(Math.random()*images.length)));
-//     if ($.inArray(imageLocation, randomImageArray) == -1) {
-//       randomImageArray.push(newCard);
-//     } else {
-//       i--;
-//     }
-//   }
-//   setTiles();
-//   console.log(randomImageArray);
-// };
+  //
+  // function getRandomImage() {
+  //   for (i=0; i<images.length; i++) {
+  //     console.log(images[Math.floor(Math.random() * images.length-1)]);
+  //   }
+  // }
+  // randomImageArray =[];
+  //
+  // var setTiles = function () {
+  //   for (var i=0; i<images.length; i++) {
+  //     var imageLocation = ((Math.floor(Math.random()*images.length)));
+  //     if ($.inArray(imageLocation, randomImageArray) == -1) {
+  //       randomImageArray.push(newCard);
+  //     } else {
+  //       i--;
+  //     }
+  //   }
+  //   setTiles();
+  //   console.log(randomImageArray);
+  // };
 });
