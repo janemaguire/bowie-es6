d2f7655b42dc0449ab0023e629735b0d757a4e8d
Jane Maguire <jane.m.maguire@gmail.com>
2016-10-11 17:34:17 +0100

    Added notes to understand which lines do what, deleted unnecessary code

--- js/app.js
+++ js/app.js
@@ -1,73 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // randomly lay out images
 
-  //lists but not randomly
-
-  // images = ["red", "blue", "green", "yellow", "pink", "purple"];
-
-  // for (i=0; i<12; i++) {
-  //   var randomImages = images[Math.floor(Math.random) * 6];
-  //   console.log(images[i]);
-  // }
-  //
-  // images = ["red", "blue", "green", "yellow", "pink", "purple"];
-  //
-  // for (i=0; i<12; i++) {
-  //   var randomImages = images[Math.floor(Math.random) * 6];
-  //   console.log(images[i], images[i], images[i]);
-  // }
-
-
-  // console.log(images[Math.floor(Math.random) * 12]);
-
-  // console.log(images[3]);
-
-  // function getRandomImage() {
-  //   for (i=0; i<12; i++) {
-  //     console.log(images[Math.floor(Math.random() * images.length-1)]);
-  //     }
-  //   }
-
-  // getRandomImage(images);
-  //
-  // function getRandomImage() {
-  //   for (i=0; i<12; i++) {
-  //     document.getElementById('a').innerHTML = (images[Math.floor(Math.random() * images.length-1)]);
-  //     }
-  //   }
-  //
-  // getRandomImage(images);
-
-
-  // document.getElementsByClass('.button').innerHTML ="red";
-
-  // var a = document.getElementById('a');
-  //
-  // a.innerHTML = 'hello';
-
-  // function Image() {
-  //   this.name = name;
-  //   this.src = src;
-  // }
-  //
-  // var trousers = new Image ('trousers', 'images/trousers.html');
-  // var eyes = new Image ('eyes', 'images/eyes.html');
-  // var labyrinth = new Image ('labyrinth', 'images/labyrinth.html');
-  // var alien = new Image ('alien', 'images/alien.html');
-  // var aladdin_sane = new Image ('aladdin_sane', 'images/aladdin_sane');
-  // var duke = new Image ('duke', 'images/duke.html');
-  //
-  // document.getElementsByClass('button').innerHTML = '';
-
+  // Fisher Yates Shuffle
   function shuffle(array) {
     var m = array.length, t, i;
-
     // While there remain elements to shuffle…
     while (m) {
-
       // Pick a remaining element…
       i = Math.floor(Math.random() * m--);
-
       // And swap it with the current element.
       t = array[m];
       array[m] = array[i];
@@ -80,34 +19,34 @@ document.addEventListener("DOMContentLoaded", function() {
   var wrapper = document.getElementById('wrapper');
   var images = ["alien", "duke", "eyes", "labyrinth", "jack", "ziggy", "blackstar", "coat", "alien", "duke", "eyes", "labyrinth", "jack", "ziggy", "blackstar", "coat"];
   images = shuffle(images);
-  console.log(images);
   var counter = 1;
   var moves = [];
   var clickedElements = [];
 
-  // create buttons
-
+  // Create buttons, set a data attribute and add event listener
   for (i=0; i<images.length; i++) {
     var newButton = document.createElement("button");
-    // newButton.innerHTML = "<img src='images/back.jpg'>";
     newButton.setAttribute('data-img', images[i]);
     newButton.addEventListener('click', function() {
-      var selection = this.getAttribute('data-img');
 
+      // On click use data attribute to push selected data into moves array and selected button into clickedElements array
+      var selection = this.getAttribute('data-img');
       moves.push(selection);
       clickedElements.push(this);
-      console.log("MOVES:", moves);
+
+      // If the button is empty, assign an image based on it's data attribute
       if (!this.innerHTML) {
         this.innerHTML = "<a class='clicked' href='#'><img src='images/" + selection + ".jpg'></a>";
-        // var randomIndex = Math.floor(Math.random() * images.length-1);
-        // var randomImage = images.splice(randomIndex, 1);
 
+        // If it is an even numbered click
         if (counter % 2 === 0 ) {
+
+          // If the data attributes of the two clicked buttons are the same, empty moves, empty clickedElements
           if(moves[0] === moves[1] ) {
-            console.log("match");
             moves = [];
             clickedElements = [];
 
+          // If they do not match, clear images after one second, empty moves array, empty clickedElements array
           } else {
             setTimeout(function() {
               clickedElements[0].innerHTML = "";
@@ -116,27 +55,28 @@ document.addEventListener("DOMContentLoaded", function() {
               clickedElements = [];
             }, 1000);
           }
+
+          // Select buttons, if all 16 are displayed as part of a match, display the winning image
           var matchedElements = document.getElementsByClassName('clicked');
           if (matchedElements.length === 16) {
             var result = document.getElementById('result');
             result.innerHTML = "Well done, contact made!" + "<img src='https://1.bp.blogspot.com/-MpnaAEObdws/VyfEFRLtbyI/AAAAAAAADxM/bDJN44Vvgf0u3KxcqnY2gcPzLmTofPDPwCKgB/s1600/29.%2B%25E2%2580%259CI%2Bdon%2527t%2Bknow%2Bwhere%2BI%2527m%2Bgoing%2Bfrom%2Bhere%252C%2Bbut%2BI%2Bpromise%2Bit%2Bwon%2527t%2Bbe%2Bboring.%25E2%2580%259D%2B%25E2%2580%2595%2BDavid%2BBowie.JPG'>";
-            console.log("winner");
           }
 
-
         } else {
-          console.log("first click");
+          // This is is the odd numbered click
         }
-
+        // Iterate counter
         counter++;
       }
     });
+    // Add buttons created inside wrapper
     wrapper.appendChild(newButton);
 
   }
+    // Reset button, if clicked reload page
     var resetButton = document.getElementById("reset");
     resetButton.addEventListener('click', function() {
       location.reload();
     });
-
 });
